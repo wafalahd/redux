4961ccdff16743b7313da3d3e6057ac42979b181
wafalahd <lahd.wafa@gmail.com>
2023-05-29 14:11:13 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AddTodo } from './components/AddTodo';
+import { TodoLists } from './components/TodoLists';
+
+const App = () => {
+  return (
+    <div className="container p-4 mt-2">
+      <h2>Todo Application</h2>
+      <AddTodo />
+      <TodoLists />
+    </div>
+  );
+}
+
+export default App;
